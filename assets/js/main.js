1e2c70a86ef5e706cb462a40ad2ac940eb8c5283
skyespirates <akashiseijurou414@gmail.com>
2023-05-29 21:00:45 +0700

    add about, skills and services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,42 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalClose = document.querySelectorAll(".services__modal-close");
 
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
-/* Link active work */ 
-
+/* Link active work */
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
+/*=============== LIGHT DARK THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
