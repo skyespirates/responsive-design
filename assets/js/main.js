75887790b27acb6ace03ac801a993dd67d306f34
skyespirates <akashiseijurou414@gmail.com>
2023-05-30 20:37:50 +0700

    add project and slide section

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,10 +30,43 @@ modalClose.forEach((mc) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 
 /* Link active work */
+const linkWork = document.querySelectorAll(".work__item");
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    576: {
+      slidesPreview: 2,
+    },
+    768: {
+      slidesPreview: 2,
+      spaceBetween: 48,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
